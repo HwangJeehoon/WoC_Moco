22d814ab9a4b8f9c03fcc065e8e0599ca4a34f65
jaejinkim121 <gisgen9@snu.ac.kr>
2026-05-11 11:33:54 +0900

    Add script for metric analysis

---
+++ script/run_metric_analysis.m
@@ -0,0 +1,10 @@
+close all;
+clc;
+pathCurrent= fileparts(mfilename('fullpath'));
+pathMain = pathCurrent + "/../src/data_analysis";
+addpath(pathMain);
+
+% moco_git\result 폴더 위치 지정
+pathResult = "E:\OneDrive-JJ\OneDrive\Biomechanical parameter optimization based wearable robot control\moco\moco_git\result";
+
+get_metric(pathResult);
